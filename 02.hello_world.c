06cfd3c7f29fe3d6e4b6188937f2f6ca654d3894
minscv <minscv@outlook.com>
2023-12-20 16:37:04 +0800

    init project

---
+++ 02.hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World! %d\n", sizeof (int *));
+    return 0;
+}
---
+++ 03.debug_programe.c
@@ -0,0 +1,9 @@
+//
+// Created by minsc on 2023/12/20.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World! %d\n", sizeof (int *));
+    return 0;
+}
